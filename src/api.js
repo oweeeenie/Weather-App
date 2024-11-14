4d507531d6ca657f53208407a810a493159fc4f5
oweeeenie <owensullivan419@yahoo.com>
2024-11-14 01:38:08 -0500

    addded some data in api.js, tried some other code but ended up restarting again, get some more progress TOMORROW!

--- src/api.js
+++ src/api.js
@@ -1,1 +1,17 @@
-// https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=10c23a9273ad387f646309ba05c1d7b1
+const fetchWeatherData = async (location) => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=10c23a9273ad387f646309ba05c1d7b1&units=metric`
+    );
+    if (!response.ok) {
+      throw new Error(`Location not found: ${response.statusText}`);
+    }
+    const weatherData = await response.json();
+    return weatherData;
+  } catch (error) {
+    console.log('error fetching weather', error);
+    throw error;
+  }
+};
+
+export { fetchWeatherData };
