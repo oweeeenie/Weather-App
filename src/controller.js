8de5ff128cc89861b5b0a1366860d46aed75f4a8
oweeeenie <owensullivan419@yahoo.com>
2024-11-14 18:58:03 -0500

    got a lot of the functionality done, about to start the actual UI portion of the popups/details

--- src/controller.js
+++ src/controller.js
@@ -1,1 +1,8 @@
+import { fetchWeatherData } from './api';
 
+async function handleWeatherSearch(location) {
+  const weatherData = await fetchWeatherData(location);
+  console.log(weatherData);
+}
+
+export { handleWeatherSearch };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles.css';
+import { handleWeatherSearch } from './controller';
+
+const weatherForm = document.querySelector('.weather-form');
+const weatherInput = document.querySelector('#location');
+
+weatherForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let userInput = weatherInput.value;
+  handleWeatherSearch(userInput);
+});
